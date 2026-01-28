3357ec315f491f8052a153e96286d92d72426479
Pedro Lopes <pedrolopes.programador013@gmail.com>
2026-01-28 15:36:51 -0300

    feat: Desenvolvido a possibilidade de criar arquivos de texto pela aplicação de console

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("O que voce deseja fazer?");
+            Console.WriteLine("1- Abrir um arquivo;");
+            Console.WriteLine("2- Criar um arquivo;");
+            Console.WriteLine("0- Sair;");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0:
+                    System.Environment.Exit(0);
+                    break;
+                case 1:
+                    OpenFile();
+                    break;
+                case 2:
+                    CreateFile();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+            
+        }
+
+        static void OpenFile()
+        {
+        }
+
+        static void CreateFile()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair):");
+            Console.WriteLine("================");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+                
+            }while (Console.ReadKey().Key != ConsoleKey.F12);
+
+            SaveFile(text);
+            
+        }
+
+        static void SaveFile(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho para salvar o arquivo: ");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Thread.Sleep(1500);
+            Menu();
+        }
+    }
+}
