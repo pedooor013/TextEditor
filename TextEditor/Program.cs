0960946c4de7cb6bd363b1115da326653f2991b6
Pedro Lopes <pedrolopes.programador013@gmail.com>
2026-01-28 15:40:50 -0300

    feat: desenvolvimento da funcao de ler o conteudo que há dentro de arquivos

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -40,6 +40,19 @@ namespace TextEditor
 
         static void OpenFile()
         {
+            Console.Clear();
+            Console.WriteLine("Digite o caminho do arquivo:");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
         }
 
         static void CreateFile()
